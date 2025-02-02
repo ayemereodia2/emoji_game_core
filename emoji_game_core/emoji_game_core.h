3e28551dcf244b8278e1405871c069fc06693eeb
ayemereodia2 <odiadavid2@gmail.com>
2025-02-02 08:56:41 -0400

    Initial Commit

---
+++ emoji_game_core/emoji_game_core.h
@@ -0,0 +1,18 @@
+//
+//  emoji_game_core.h
+//  emoji_game_core
+//
+//  Created by ANDELA on 02/02/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for emoji_game_core.
+FOUNDATION_EXPORT double emoji_game_coreVersionNumber;
+
+//! Project version string for emoji_game_core.
+FOUNDATION_EXPORT const unsigned char emoji_game_coreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <emoji_game_core/PublicHeader.h>
+
+
---
+++ emoji_game_coreTests/emoji_game_coreTests.swift
@@ -0,0 +1,17 @@
+//
+//  emoji_game_coreTests.swift
+//  emoji_game_coreTests
+//
+//  Created by ANDELA on 02/02/2025.
+//
+
+import Testing
+@testable import emoji_game_core
+
+struct emoji_game_coreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
